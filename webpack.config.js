90d59e991e035dc78167af1c82b9e7a9e37bd4bd
theuniparse <theuniparse@gmail.com>
2022-04-28 03:47:20 +0100

    v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
   mode: 'development',
   entry: {
@@ -53,6 +54,7 @@ module.exports = {
       title: 'webpack app',
       filename: 'index.html',
       template: 'src/template.html'
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ]
 }
